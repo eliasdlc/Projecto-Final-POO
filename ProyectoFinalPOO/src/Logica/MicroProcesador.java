c35fd7a0cb891bd3ca3ca5f975fab117a727f806
Uni <Uni@Yuji>
2024-07-09 18:38:35 -0400

    Creacion de la clase hija MicroProcesador

---
+++ ProyectoFinalPOO/src/Logica/MicroProcesador.java
@@ -0,0 +1,61 @@
+package Logica;
+
+public class MicroProcesador extends Componente {
+	private float velocidad;
+	private String tipoConexion;
+	private String plataforma;
+	private int cantNucleo;
+	private float energia;
+	
+	public MicroProcesador(String id, String marca, String modelo, float precio, int cantidadDisp, float velocidad, String tipoConexion, String plataforma, int cantNucleo, float energia) {
+		super(id, marca, modelo, precio, cantidadDisp);
+		this.velocidad = velocidad;
+		this.tipoConexion = tipoConexion;
+		this.plataforma = plataforma;
+		this.cantNucleo = cantNucleo;
+		this.energia = energia;
+	}
+
+	public float getVelocidad() {
+		return velocidad;
+	}
+
+	public void setVelocidad(float velocidad) {
+		this.velocidad = velocidad;
+	}
+
+	public String getTipoConexion() {
+		return tipoConexion;
+	}
+
+	public void setTipoConexion(String tipoConexion) {
+		this.tipoConexion = tipoConexion;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public int getCantNucleo() {
+		return cantNucleo;
+	}
+
+	public void setCantNucleo(int cantNucleo) {
+		this.cantNucleo = cantNucleo;
+	}
+
+	public float getEnergia() {
+		return energia;
+	}
+
+	public void setEnergia(float energia) {
+		this.energia = energia;
+	}
+
+	
+	
+}
