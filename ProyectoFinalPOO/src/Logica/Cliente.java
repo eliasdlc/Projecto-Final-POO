181a8b5d3c872cacbb42ec5c369a24738a7b5b46
Uni <Uni@Yuji>
2024-07-09 17:30:45 -0400

    Creacion de la clase cliente

---
+++ ProyectoFinalPOO/src/Logica/Cliente.java
@@ -0,0 +1,37 @@
+package Logica;
+
+public class Cliente {
+	private String idclliente;
+	private String nombre;
+	private String correo;
+	private String desc;
+	public Cliente(String idclliente, String nombre, String correo, String desc) {
+		super();
+		this.idclliente = idclliente;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.desc = desc;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getIdclliente() {
+		return idclliente;
+	}
+	
+}
