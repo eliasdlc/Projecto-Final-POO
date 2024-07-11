3ab671082ceae4779bcd931376defc78da323703
Uni <Uni@Yuji>
2024-07-11 17:14:15 -0400

    Arreglo de detalles

--- ProyectoFinalPOO/src/Logica/Factura.java
+++ ProyectoFinalPOO/src/Logica/Factura.java
@@ -3,34 +3,42 @@ package Logica;
 public class Factura {
 	
 	private String idCliente;
-	private String idComputadora;
 	private String id;
 	private Float montoTotal;
 	private boolean comprado;
+	
+	public Factura(String idCliente, String id, Float montoTotal, boolean comprado) {
+		super();
+		this.idCliente = idCliente;
+		this.id = id;
+		this.montoTotal = montoTotal;
+		this.comprado = comprado;
+	}
+	
 	public String getIdCliente() {
 		return idCliente;
 	}
+	
 	public void setIdCliente(String idCliente) {
 		this.idCliente = idCliente;
 	}
-	public String getIdComputadora() {
-		return idComputadora;
-	}
-	public void setIdComputadora(String idComputadora) {
-		this.idComputadora = idComputadora;
-	}
+	
 	public String getId() {
 		return id;
 	}
+	
 	public Float getMontoTotal() {
 		return montoTotal;
 	}
+	
 	public void setMontoTotal(Float montoTotal) {
 		this.montoTotal = montoTotal;
 	}
+	
 	public boolean isComprado() {
 		return comprado;
 	}
+	
 	public void setComprado(boolean comprado) {
 		this.comprado = comprado;
 	}
